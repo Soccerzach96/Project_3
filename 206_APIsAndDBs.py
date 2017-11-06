76943573ecb2d3b11d9c47584a9415ba214fdae9
Zach <strongz@umich.edu>
2017-11-05 20:39:57 -0500

    Completed Task 1

--- 206_APIsAndDBs.py
+++ 206_APIsAndDBs.py
@@ -49,18 +49,32 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 CACHE_FNAME = "206_APIsAndDBs_cache.json"
 # Put the rest of your caching setup here:
-
-
+try:
+	cache_file = open(CACHE_FNAME, 'r')
+	cache_contents = cache_file.read()
+	CACHE_DICTION = json.loads(cache_contents)
+	cache_file.close()
+except:
+	CACHE_DICTION = {}
 
 # Define your function get_user_tweets here:
-
-
-
+def get_user_tweets(user):
+	if user in CACHE_DICTION:
+		print("Data was in the cache")
+		return CAHCE_DICTION[user]
+	else:
+		print("Making a request for new data...")
+		results = api.user_timeline(user)
+		CACHE_DICTION[user] = results
+		f = open(CAHCE_FNAME, "w")
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+		return CACHE_DICTION[user]
 
 
 # Write an invocation to the function for the "umich" user timeline and 
 # save the result in a variable called umich_tweets:
-
+umich_tweets = get_user_tweets('@umich')
 
 
 
